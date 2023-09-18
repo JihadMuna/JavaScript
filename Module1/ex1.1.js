99aa692576613e6e04eb89a53884808badc31c81
Jihad Muna <142898308+JihadMuna@users.noreply.github.com>
2023-09-18 21:11:49 +0300

    Adding Exersice 1.1

---
+++ Module1/ex1.1.js
@@ -0,0 +1,27 @@
+//1. Create a variable whose value holds a string.
+let myName = "Jihad"; 
+console.log(myName);
+
+//2. Create a variable whose value holds a number.
+let age = 31;
+console.log(age);
+
+//3. Create a variable whose value holds an object.
+let file = Object;
+console.log(file);
+
+//4. Create a variable whose value holds a boolean.
+let apple = false;
+console.log(apple);
+
+//5. Create a variable whose value holds null.
+let x = null ;
+console.log(x);
+
+//6. Create a variable whose value is undefined.
+let y = undefined ;
+console.log(y);
+
+//7. Create a variable whose value holds a symbol.
+let dollar = Symbol("$");
+console.log(dollar);
