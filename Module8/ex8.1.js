35194a3be61e8b7f1baa133565f8be2379ae4306
Jihad Muna <142898308+JihadMuna@users.noreply.github.com>
2023-09-19 10:35:59 +0300

    Adding Exersice 8.1

---
+++ Module8/ex8.1.js
@@ -0,0 +1,10 @@
+const book = {
+    title: "learning JavaScript",
+    authorName: "Yishai",
+    publishingYear: 2023,
+    language: "English",
+    price: 100
+}
+
+//‘The book <book name> was written by <author name> in the year <publishing year>’
+console.log('The book',book['title'],'was written by',book['authorName'],'in the year',book['publishingYear'] );
