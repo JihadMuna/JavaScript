93d359d060c71f9ddef193e2da6873b1e563dc59
Jihad Muna <jojo.muna.2019@gmail.com>
2023-09-20 11:39:51 +0300

    Adding Exersice 2.3

---
+++ Module2/ex2.3.js
@@ -0,0 +1,8 @@
+const radius = 10;
+
+function circleArea(radius){
+return (Math.PI * Math.pow(radius,2));
+};
+
+console.log(circleArea(radius));
+console.log(circleArea(radius).toFixed(2));
--- Module2/ex32.3.js
+++
@@ -1,5 +0,0 @@
-const radius=10;
-const pi=Math.PI;
-function circleArea(radius,pi){
-return pi * radius * radius
-};
