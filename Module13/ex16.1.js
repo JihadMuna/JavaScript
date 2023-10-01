ebae9c5c0e59e4958aad9a7be3ff6f138b7df319
Jihad Muna <jojo.muna.2019@gmail.com>
2023-10-01 16:39:31 +0300

    Add Module16

--- Module13/ex16.1.js
+++
@@ -1,9 +0,0 @@
-function funcA() {
-    console.log(a); // output will be undefined, because a is declared by var, but it still not has initialization.
-    console.log(foo()); // output will be 2, because its an inner function (foo()) at the outer function (func()).
-    var a = 1;
-    function foo() {
-    return 2;
-    }
-    }
-    funcA();
