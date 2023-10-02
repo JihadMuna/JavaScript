aa9cacd3d5141f2b53010d0999eda79e82de85fa
Jihad Muna <jojo.muna.2019@gmail.com>
2023-10-02 22:58:05 +0300

    Add ex19.4

---
+++ Module19/ex19.3.js
@@ -0,0 +1,11 @@
+
+console.log(document);
+
+const h1 = document.getElementById("head");
+
+
+const paragraphs = document.getElementsByName("P");
+console.log('paragraphs', paragraphs);
+
+const footer = document.getElementById("foot");
+
