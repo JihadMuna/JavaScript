43c970461532896463a5ba30991a6a281d43a452
Jihad Muna <142898308+JihadMuna@users.noreply.github.com>
2023-11-05 13:18:27 +0200

    Add Ex23.2

---
+++ Module23/ex23.2.js
@@ -0,0 +1,18 @@
+//Task:
+// Edit the storm object.
+// Invoke the printSuperPower function using the storm object as the context of the function.
+// So your output should be: my superpower is flying Well,
+// Storm also controls the weather,
+// so, whatever you choose!
+// You cannot change the printSuperPower function
+
+const storm = {
+    superPower: "flying",
+    printSuperPower:printSuperPower
+};
+
+function printSuperPower() {
+console.log("my superpower is " + this.superPower);
+}
+
+storm.printSuperPower();
