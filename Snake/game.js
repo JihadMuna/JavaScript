5177840151ed77f7daf00aceada7c2d4f4464ddf
JihadMuna <jojo.muna.2019@gmail.com>
2023-11-07 09:18:42 +0200

    Adding Snake Game

--- Snake/game.js
+++ Snake/game.js
@@ -1,25 +1,52 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake.js";
+const gameBoard = document.querySelector('#gameBoard');
+const ctx = gameBoard.getContext('2d');
+const scoreText = document.querySelector('#scoreText');
+const resetBtn = document.querySelector('#resetBtn');
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = 'white';
+const snakeColor = 'lightgreen';
+const snakeBoarder = 'black';
+const fooColor = 'red';
+const unitSize = 25;
+let running = 'false';
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+let snake = [
+    {x: unitSize * 4 , y: 0},
+    {x: unitSize * 3 , y: 0},
+    {x: unitSize * 2 , y: 0},
+    {x: unitSize , y: 0},
+    {x:0, y:0}
+];
 
-let lastRenderTime = 0;
-const gameBoard = document.querySelector('#game-board');
+window.addEventListener('keydown', changeDirection);
+resetBtn.addEventListener('click', resetGame);
 
-function main(currentTime) {
-window.requestAnimationFrame(main);
-const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+gameStart();
 
-lastRenderTime = currentTime;
+function gameStart(){};
+function nextTick(){};
+function clearBoard(){};
+function createFood(){
+    function randomFood(min, max){
+        const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
+return randNum;
+    }
+    foodX = randomFood(0, gameWidth - unitSize);
+    foodY = randomFood(0, gameWidth - unitSize);
+    
+};
+function drawFood(){
+    ctx.fillStyke
+};
+function moveSnack(){};
+function drawSnake(){};
+function changeDirection(){};
+function checkGameOver(){};
+function displayGameOver (){};
+function resetGame(){};
 
-update();
-draw(); 
-}
-
-window.requestAnimationFrame(main);
-
-function update() {
-updateSnake();
-}
-
-function draw() {
-    drawSnake(gameBoard);
-}
